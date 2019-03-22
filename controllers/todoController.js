bcb2c6e83c493091eee7148abc5e3cd16c124453
th0r0nd0r <pjfritch1@gmail.com>
2019-03-22 14:09:03 -0700

    add options to get controller

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,12 @@
+const data = [
+  {item: 'slurp and glurp'},
+  {item: 'munch a bunch'},
+  {item: 'snooze and cruise'},
+];
+
 export default function (app) {
   app.get('/todo', function (req, res) {
-    res.render('todo');
+    res.render('todo', {todos: data});
   });
 
   app.post('/todo', function (req, res) {
